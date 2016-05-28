4e187dc8ee32d4d62f0e92bf595159d2d00a9421
tr3mor <storozhenkopf@gmail.com>
2016-05-28 14:57:29 +0300

    28.05.16

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # matches-in-2-files
+parcing to files with lists(pc names for example) and find matches between them.
--- matches.py
+++ matches.py
@@ -2,19 +2,18 @@ import argparse
 import re
 
 
-def ParsArgs():
+def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f1', '-F1', nargs='?', help="Path to txt file with list of pcs")
     parser.add_argument('-f2', '-F2', nargs='?', help="path to txt file with list of pcs#2")
     return parser
 
 
-def ParsFile(path):
+def parse_file(path):
     # take info from file
     file = open(path, 'r')
     list_pcs = []
     for line in file:
-        # print(line[1:])
         line = re.sub("^\s+|\n|\r|\s+$", '', line)
         if (line != "\n") & (line != ''):
             list_pcs.append(line.upper())
@@ -23,10 +22,10 @@ def ParsFile(path):
 
 
 def main():
-    parser = ParsArgs()
+    parser = parse_args()
     namespace = parser.parse_args()
-    list1 = ParsFile(namespace.f1)
-    list2 = ParsFile(namespace.f2)
+    list1 = parse_file(namespace.f1)
+    list2 = parse_file(namespace.f2)
     a = [a for a in list1 if a in list2]
     for x in a:
         print(x)
